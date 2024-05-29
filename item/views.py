8df848f827d909ed126e0b61bd79aaf0352006ce
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-05-30 01:53:51 +0630

    search function

--- item/views.py
+++ item/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from .models import item as ObjItem
+from django.db.models import Q
+from .models import item as ObjItem , category
 from django.contrib.auth.decorators import login_required
 from .forms import NewItemForm, EditItemForm
 
-# Create your views here.
+
+def items(request):
+    query = request.GET.get('query','')
+    category_id = request.GET.get('category', 0)
+    categories = category.objects.all()
+    items = ObjItem.objects.filter(is_sold=False)
+    
+    if category_id:
+        items = items.filter(category_id=category_id)
+    
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+        
+    return render(request, 'item/items.html', {
+        'items': items,
+        'query': query,
+        'categories' : categories,
+        'category_id' : int(category_id)
+    })
+    
 def detail(request, pk):
     item = get_object_or_404(ObjItem, pk=pk)
     related_items = ObjItem.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
